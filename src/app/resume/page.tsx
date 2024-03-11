6cb938aafeb48c473940ae622145ce7b5ed28857
DurgaPrasadSwain <durgaprasadswain@gmail.com>
2024-03-12 01:22:20 +0530

    update 1.0.0

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import res from '../../../public/Screenshot 2024-03-10 234458.png'
+import res from '../../../public/Screenshot2024-03-10234458.png'
 
 
 const page = () => {
